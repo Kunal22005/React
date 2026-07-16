53a4d3c04c0f905fb59e05d5bbe337d82c93a8bf
Kunal Sharma <kunalsharma221726@gmail.com>
2026-07-16 18:49:49 +0530

    Updated

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
     <Chai/>
-    {/* <h1>Chai aur Code | Kunal Sharma</h1> */}
+    {/* <h1>Chai and Code | Kunal Sharma</h1> */}
     </>
   );
 }
---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Hello, World!");
+let username
+ = "Alice";
+console.log("My name is " + username);
+let age = 30;
+console.log("I am " + age + " years old.");
+
+function greet(person) {
+
+    return "Hello, " + person + "!";
+}
+console.log(greet(username));
+let numbers = [1, 2, 3, 4, 5];
+console.log("Numbers: " + numbers);
+let sum = numbers.reduce((a, b) => a + b, 0);
+console.log("Sum of numbers: " + sum);
+
+let person = {
+    name: "Bob",
+    age: 25,
+    greet: function() {
+        return "Hi, I'm " + this.name + " and I'm " + this.age + " years old.";
+    }           
+};
+console.log(person.greet());
+console.log(person["name"]);
